c8fa098604b31e02cf4250fa5148c7d9ef69d581
Egor Komarov <egor_komarov@epam.com>
2019-10-28 16:10:34 +0300

    rechecked setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-name="snapshot",
-packages=find_packages(),
-scripts=["snapshot.py"],
-version="2.0",
-author="Egor Komarov",
-author_email="egor_komarov@epam.com",
-description="sysinfo console utility",
-#long_description=long_description,
-long_description_content_type="text/markdown",
-license="EPAM",
-python_requires='>=3.7.5',
-install_requires=['psutil'])
+    name="snapshot",
+    packages=find_packages(),
+    scripts=["snapshot.py"],
+    version="2.0",
+    author="Egor Komarov",
+    author_email="egor_komarov@epam.com",
+    description="sysinfo console utility",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="EPAM",
+    python_requires='>=3.7.5',
+    install_requires=['psutil']
+)
