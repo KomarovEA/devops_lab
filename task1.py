41cc046c8eb77bc0aeae8b6dfe007ee68d8ea067
Egor Komarov <egor_komarov@epam.com>
2019-10-29 11:40:33 +0300

    fix task1 and task4

--- task1.py
+++ task1.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # encoding=utf8
-N = int(input("Input int N: "))
-if (N > 0) & (N < 21):
-    print("N^2 is:", N * N)
+N: int = int(input("Input int N: "))
+if 0 < N < 21:
+    i = 1
+    while i < N:
+        print(i, '^2 is:', i * i)
+        i += 1
 else:
     print("N must between 1 and 20")
+    exit(1)
+
