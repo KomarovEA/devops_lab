eb6cfd4d43d7eed6b4929f18d40e738d9068a529
Egor Komarov <egor_komarov@epam.com>
2019-10-29 22:05:37 +0300

    Renew test_task6.py

--- test_task6.py
+++ test_task6.py
@@ -11,13 +11,14 @@ class TestTask6(TestCase):
     def test_SplitStr(self):
         """Test for SplitStr"""
         self.assertEqual(task6.SplitStr('the first string'), ['the', 'first', 'string'])
-        self.assertEqual(task6.SplitStr('Prosto dlinnaya stroka'), ['Prosto', 'dlinnaya', 'stroka'])
+        self.assertEqual(task6.SplitStr('Prosto dlinnaya stroka'),
+                         ['Prosto', 'dlinnaya', 'stroka'])
         self.assertEqual(task6.SplitStr(''), [''])
         self.assertEqual(task6.SplitStr('SingleWord'), ['SingleWord'])
 
     def test_ReversList(self):
         self.assertEqual(task6.ReversList(['the', 'first', 'string']), 'eht tsrif gnirts')
-        self.assertEqual(task6.ReversList(['Prosto', 'dlinnaya', 'stroka']), \
+        self.assertEqual(task6.ReversList(['Prosto', 'dlinnaya', 'stroka']),
                          'otsorP ayannild akorts')
         self.assertEqual(task6.ReversList(['']), '')
         self.assertEqual(task6.ReversList(['SingleWord']), 'droWelgniS')
